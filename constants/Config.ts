08ca87d1d7e56b9fb180c254d6bc832b317b67f2
SardarHussain65 <sardar6hussain5@gmail.com>
2026-05-26 13:08:43 +0500

    chagne api domain

--- constants/Config.ts
+++ constants/Config.ts
@@ -4,7 +4,7 @@ import Constants from 'expo-constants';
 // export const BASE_URL = 'http://192.168.86.250:8081';
 
 // Production VPS Server:
-export const BASE_URL = 'https://api.sardarhussain.me';
+export const BASE_URL = 'https://api.apnaustad.app';
 
 export const MAPTILER_API_KEY = Constants.expoConfig?.extra?.maptilerApiKey || process.env.MAPTILER_API_KEY || 'o3cm0oM4Yn3L4Nyf1ydh';
 
